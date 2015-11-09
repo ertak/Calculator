eac6a7e899d5f3b230fed45d65ca9010419bd8bb
uozgen <ugurozgenn@gmail.com>
2015-11-09 12:09:35 +0200

    --

--- Calculator.go
+++ Calculator.go
@@ -1,1 +1,21 @@
 package CalculatorZFU
+
+func sum(x, y float64) float64 {
+	x = x + y
+	return x
+}
+
+func sub(x, y float64) float64 {
+	x = x - y
+	return x
+}
+
+func div(x, y float64) float64 {
+	x = x / y
+	return x
+}
+
+func mul(x, y float64) float64 {
+	x = x * y
+	return x
+}
